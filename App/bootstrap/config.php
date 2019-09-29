f130b9f2a9a6c4ef8336eab8174ed502d5ce0c46
nbrabant <brabantnicolas59@gmail.com>
2019-09-29 21:39:00 +0200

    fix dependencies injections

--- App/bootstrap/config.php
+++ App/bootstrap/config.php
@@ -1,11 +1,21 @@
 <?php
 
-use App\Game\Game;
 use function DI\create;
+use App\Game;
+use App\Game\Bot;
 use App\Client\LightningbotClient;
+use Psr\Log\LoggerInterface;
+use Monolog\Logger;
+use GuzzleHttp\Client;
 
 return [
-    LightningbotClient::class => create(LightningbotClient::class),
+    Client::class => create(Client::class),
+    LoggerInterface::class => create(Logger::class)
+        ->constructor('debug'),
+    
+    LightningbotClient::class => create(LightningbotClient::class)
+        ->constructor(\DI\get(Client::class)),
 
     Game::class => create(Game::class),
+    MyBot::class => create(MyBot::class),
 ];
