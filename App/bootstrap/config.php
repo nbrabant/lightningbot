0a4e0633cd043a4fc3af6d86ada7c506290c5e4b
nbrabant <brabantnicolas59@gmail.com>
2019-10-24 03:32:05 +0200

    fix monolog instanciation on DI

--- App/bootstrap/config.php
+++ App/bootstrap/config.php
@@ -11,7 +11,7 @@ return [
     Client::class => create(Client::class)
         ->constructor(['verify' => false]),
     LoggerInterface::class => create(Logger::class)
-        ->constructor('lightningbot', new StreamHandler('php://stdout', Logger::DEBUG)),
+        ->constructor('lightningbot', [new StreamHandler('php://stdout', Logger::DEBUG)]),
 
     LightningbotClient::class => create(LightningbotClient::class)
         ->constructor(\DI\get(Client::class), \DI\get(LoggerInterface::class)),
